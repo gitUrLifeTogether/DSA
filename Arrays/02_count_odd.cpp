6ce632b5a2df0884368991e96ded8186490a72d6
gitUrLifeTogether <sandhaletech@gmail.com>
2026-03-27 11:54:47 +0530

    counting odd numbers in an array

---
+++ Arrays/02_count_odd.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int countOdd(int arr[], int n){
+        int count=0;
+        for (int i=0;i<n;i++){
+            if(arr[i]%2==1){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+int main() {
+    Solution sol;
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int count = sol.countOdd(arr, n);
+    cout << "Count of odd numbers: " << count << endl;
+    return 0;
+}

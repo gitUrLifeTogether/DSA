1870ffb04a03fc7168cf9848271e7de5deb8890d
gitUrLifeTogether <sandhaletech@gmail.com>
2026-03-27 18:39:03 +0530

    second largest element better solution

---
+++ Arrays/Fundamentals/07_second_largest_better.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int secondLargestElement(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2) return -1;
+        int largest = INT_MIN, secondLargest = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            largest = max(largest, nums[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > secondLargest && nums[i] != largest) {
+                secondLargest = nums[i];
+            }
+        }
+        return secondLargest == INT_MIN ? -1 : secondLargest;
+    }
+};
+
+int main() {
+    vector<int> nums = {1, 2, 4, 6, 7, 5};
+    Solution sol;
+    int ans = sol.secondLargestElement(nums);
+    cout << "The second largest element is: " << ans << endl;
+    return 0;
+}

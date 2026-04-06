55e9ef09e15b5b458ca18df1c64c80d6109b3389
gitUrLifeTogether <sandhaletech@gmail.com>
2026-04-06 13:42:15 +0530

    left rotation by 1

--- Arrays/Fundamentals/09_left_rotation.cpp
+++ Arrays/Fundamentals/09_left_rotation.cpp
@@ -4,11 +4,12 @@ using namespace std;
 class Solution {
 public:
     void rotateArrayByOne(vector<int>& nums) {
-        int temp = nums[0];
-        for(int i = 0; i < nums.size()-1; i++){
-            nums[i] = nums[i+1];
+        int temp=nums[0];
+        for(int i=1;i<nums.size();i++){
+            nums[i-1]=nums[i];
         }
-        nums[nums.size()-1] = temp;
+        nums[nums.size()-1]=temp;
+
     }
 };
 

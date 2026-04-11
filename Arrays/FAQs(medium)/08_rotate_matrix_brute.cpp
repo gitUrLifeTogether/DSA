65cbadc7b216976ce657c203cce58a7ca386d5e0
gitUrLifeTogether <sandhaletech@gmail.com>
2026-04-11 12:19:53 +0530

    rotate matrix brute force

--- Arrays/FAQs(medium)/08_rotate_matrix_brute.cpp
+++ Arrays/FAQs(medium)/08_rotate_matrix_brute.cpp
@@ -1,60 +1,52 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    /* Function to rotate the given 
-    matrix by 90 degrees clockwise*/
     void rotateMatrix(vector<vector<int>>& matrix) {
-        
-        /* Get the size of the matrix 
-        (assuming it's a square matrix)*/
         int n = matrix.size();
-        
-        // Initialize new matrix to store rotated values
-        vector<vector<int>> rotated(n, vector<int>(n, 0)); 
-        
-        // Iterate through elements of original matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                
-                /* Rotate the element to its new 
-                position in the rotated matrix
-                New position is (j, n - i - 1) 
-                in the rotated matrix*/
-                
-                rotated[j][n - i - 1] = matrix[i][j];
+
+        vector<vector<int>> rotated(n, vector<int>(n, 0));
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                rotated[j][n - 1 - i] = matrix[i][j];
             }
         }
-        
-        //copy rotated elements to matrix
-        for(int i = 0; i < rotated.size(); i++){
-            for(int j = 0; j < rotated[0].size(); j++){
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
                 matrix[i][j] = rotated[i][j];
             }
         }
-    
     }
 };
 
 int main() {
-    vector<vector<int>> arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; 
-    
-    // Create an instance of the Solution class
-    Solution sol; 
-    
-    sol.rotate(arr); 
-    
-    // Print the rotated matrix
-    cout << "Rotated Image" << endl;
-    for (int i = 0; i < arr.size(); i++) {
-        for (int j = 0; j < arr[0].size(); j++) {
-            cout << arr[i][j] << " "; 
+
+    int n;
+    cin >> n;
+
+    vector<vector<int>> arr(n, vector<int>(n));
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> arr[i][j];
         }
-        cout << endl; 
+    }
+
+    Solution sol;
+
+    sol.rotateMatrix(arr);
+
+    cout << "Rotated Matrix:" << endl;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
 
     return 0;
 }
-
-

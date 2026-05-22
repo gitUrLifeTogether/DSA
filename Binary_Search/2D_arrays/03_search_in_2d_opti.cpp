120ed3e1df5b1694adb7d327a5e332eaa1a25240
gitUrLifeTogether <sandhaletech@gmail.com>
2026-05-22 22:06:48 +0530

    search in 2d opti opti

---
+++ Binary_Search/2D_arrays/03_search_in_2d_opti.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    //Function to search for a given target in matrix
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        //Intialize the row and col
+        int row = 0, col = m - 1;
+
+        //Traverse the matrix from (0, m-1):
+        while (row < n && col >= 0) {
+            
+            //Return true of target is found
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] < target) row++;
+            else col--;
+        }
+        //Return false if target not found
+        return false;
+    }
+};
+
+int main() {
+    
+    vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, 
+                                  {2, 5, 8, 12, 19},
+                                  {3, 6, 9, 16, 22},
+                                  {10, 13, 14, 17, 24},
+                                  {18, 21, 23, 26, 30}};
+    int target = 8;
+    
+    //Create an instace of Solution class
+    Solution sol;
+    
+    bool result = sol.searchMatrix(matrix, target);
+    
+    // Output the result
+    result ? cout << "true\n" : cout << "false\n";
+    
+    return 0;
+}

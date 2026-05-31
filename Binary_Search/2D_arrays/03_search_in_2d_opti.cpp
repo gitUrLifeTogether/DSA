18f6621056f79fb6588d8fcca67b730763f56a16
gitUrLifeTogether <sandhaletech@gmail.com>
2026-05-31 22:26:28 +0530

    search in 2d optimal

--- Binary_Search/2D_arrays/03_search_in_2d_opti.cpp
+++ Binary_Search/2D_arrays/03_search_in_2d_opti.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 class Solution {
 public:
-    //Function to search for a given target in matrix
+    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        
-        //Intialize the row and col
+      
         int row = 0, col = m - 1;
 
         //Traverse the matrix from (0, m-1):
